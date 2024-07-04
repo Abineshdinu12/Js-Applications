80831c5e2358d657d2f96b686d1995a0193e5923
Abineshdinu12 <abineshdinu@gmail.com>
2024-07-04 18:37:49 +0530

    Removed-quantity

--- js/object.js
+++ js/object.js
@@ -1,14 +1,14 @@
 const products = [
-  { productName: "T-Shirt", productPrice: 100, productQuantity: 2 },
-  { productName: "Jeans", productPrice: 80, productQuantity: 3 },
-  { productName: "Sneakers", productPrice: 120, productQuantity: 1 },
-  { productName: "Watch", productPrice: 200, productQuantity: 1 },
-  { productName: "Backpack", productPrice: 50, productQuantity: 5 },
-  { productName: "Headphones", productPrice: 150, productQuantity: 2 },
-  { productName: "Sunglasses", productPrice: 70, productQuantity: 2 },
-  { productName: "Phone Case", productPrice: 20, productQuantity: 1 },
-  { productName: "Laptop", productPrice: 1200, productQuantity: 1 },
-  { productName: "Smartwatch", productPrice: 300, productQuantity: 1 },
+  { productName: "T-Shirt", productPrice: 100 },
+  { productName: "Jeans", productPrice: 80 },
+  { productName: "Sneakers", productPrice: 120 },
+  { productName: "Watch", productPrice: 200 },
+  { productName: "Backpack", productPrice: 50 },
+  { productName: "Headphones", productPrice: 150 },
+  { productName: "Sunglasses", productPrice: 70 },
+  { productName: "Phone Case", productPrice: 20 },
+  { productName: "Laptop", productPrice: 1200 },
+  { productName: "Smartwatch", productPrice: 300 },
 ];
 
 const orders = [
