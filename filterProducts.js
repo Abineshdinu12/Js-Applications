59255d7cef3b59bb95e0da1c3b46295abe4cd228
Abineshdinu12 <abineshdinu@gmail.com>
2024-07-04 16:08:28 +0530

    Completed-Product-Search

---
+++ filterProducts.js
@@ -0,0 +1,29 @@
+
+function renderProducts(products) {
+  const productList = document.getElementById("productList");
+  productList.innerHTML = ""; 
+  
+  products.map(product => {
+    const card = document.createElement("div");
+    card.classList.add("col-md-3", "mb-4");
+    card.innerHTML = `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${product.productName}</h5>
+          <p class="card-text">Price: ${product.productPrice}</p>
+          <p class="card-text">Quantity: ${product.productQuantity}</p>
+        </div>
+      </div>
+    `;
+    
+    productList.appendChild(card);
+  });
+}
+renderProducts(cart[0]);
+document.getElementById("searchInput").addEventListener("input", function() {
+  const searchQuery = this.value.toLowerCase();
+  const filteredProducts = cart[0].filter(product => 
+    product.productName.toLowerCase().includes(searchQuery)
+  );
+  renderProducts(filteredProducts);
+});
---
+++ object.js
@@ -0,0 +1,21 @@
+const products = [
+  { productName: "T-Shirt", productPrice: 100, productQuantity: 2 },
+  { productName: "Jeans", productPrice: 80, productQuantity: 3 },
+  { productName: "Sneakers", productPrice: 120, productQuantity: 1 },
+  { productName: "Watch", productPrice: 200, productQuantity: 1 },
+  { productName: "Backpack", productPrice: 50, productQuantity: 5 },
+  { productName: "Headphones", productPrice: 150, productQuantity: 2 },
+  { productName: "Sunglasses", productPrice: 70, productQuantity: 2 },
+  { productName: "Phone Case", productPrice: 20, productQuantity: 1 },
+  { productName: "Laptop", productPrice: 1200, productQuantity: 1 },
+  { productName: "Smartwatch", productPrice: 300, productQuantity: 1 },
+];
+
+const orders = [
+  { orderedProduct: "laptop", price: 120, quantity: 1 },
+  { orderedProduct: "Watch", price: 100, quantity: 2 },
+  { orderedProduct: "I Phone", price: 150, quantity: 2 },
+];
+
+const cart = [products, orders];
+// export default cart;
